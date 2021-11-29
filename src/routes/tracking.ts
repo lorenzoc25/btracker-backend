2de7c3ff88d6669eb1c88400c9eb5db45e461590
Mark Li <limarksmail@gmail.com>
2021-11-28 21:28:26 -0800

    User & JWT Integration (#6)
    Add the tracking information into the user tables

--- src/routes/tracking.ts
+++ src/routes/tracking.ts
@@ -1,22 +1,49 @@
 import express, {
-  Request,
   Response,
 } from 'express';
 import getrackingFromDB from '../functions/getTrackingFromDB';
 import updateTracking from '../functions/updateTracking';
+import { AuthRequest } from '../../types/auth';
+import { PackageModel } from '../schema/packageSchema';
+import { UserModel } from '../schema/userSchema';
 
 const router = express.Router();
 
 router.get('/:trackingId', async (
-  req: Request,
+  req: AuthRequest,
   res: Response,
 ) => {
+  if (
+    req.auth === undefined
+    || req.auth.email === undefined
+  ) {
+    res.status(403).json({
+      message: 'The access token is missing or invalid',
+    });
+    return;
+  }
+
   const trackingNum: string = req.params.trackingId;
+  const userEmail = req.auth.email;
+
+  await UserModel.findOneAndUpdate(
+    {
+      email: userEmail,
+    },
+    {
+      $push: {
+        packageList: trackingNum,
+      },
+    },
+  );
 
   const response = await getrackingFromDB(trackingNum);
 
   if (response.length === 0
     || Date.now() - response[0].lastUpdate > 1800) {
+    await PackageModel.deleteMany({
+      tracking: trackingNum,
+    });
     await updateTracking(trackingNum);
     res.status(200).send(await getrackingFromDB(trackingNum));
   } else {
